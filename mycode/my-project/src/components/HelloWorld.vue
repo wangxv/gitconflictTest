cd43c965b3eda435a28739fba455830011281abb
wangxuke <xuke,wang@colorcun.com>
2018-08-05 21:19:59 +0800

    w-
    首页画完，但是没有功能

--- mycode/my-project/src/components/HelloWorld.vue
+++ mycode/my-project/src/components/HelloWorld.vue
@@ -1,43 +1,20 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-    </ul>
+    <index-vue></index-vue>
   </div>
 </template>
 
 <script>
+import indexVue from './wcomponents/index.vue';
 export default {
   name: 'HelloWorld',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+  components:{
+    indexVue
   }
 }
 </script>
--- mycode/my-project/src/components/wcomponents/index.vue
+++ mycode/my-project/src/components/wcomponents/index.vue
@@ -1,29 +1,167 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <router-link>
-
-    </router-link>
-    <router-view>
-
-    </router-view>
+  <el-container style="height: 500px;">
+    <el-aside width="200px">
+        <el-menu
+          default-active="2"
+          class="el-menu-vertical-demo"
+          @open="handleOpen"
+          @close="handleClose"
+          background-color="#000"
+          text-color="#fff"
+          active-text-color="#ffd04b">
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>配置列表</span>
+            </template>
+            <el-menu-item-group>
+              <template slot="title">商品配置</template>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="分组2">
+              <el-menu-item index="1-3">活动配置</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="1-4">
+              <template slot="title">选项4</template>
+              <el-menu-item index="1-4-1">选项1</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-menu-item index="2">
+            <i class="el-icon-menu"></i>
+            <span slot="title">活动列表</span>
+          </el-menu-item>
+          <el-menu-item index="3" >
+            <i class="el-icon-document"></i>
+            <span slot="title">商品列表</span>
+          </el-menu-item>
+          <el-menu-item index="4">
+            <i class="el-icon-setting"></i>
+            <span slot="title">用户列表</span>
+          </el-menu-item>
+        </el-menu>
+    </el-aside>
+    <el-container>
+      <el-header style="text-align: right; font-size: 12px">
+         <el-row>
+           <el-col :span="6">
+             <el-date-picker
+                v-model="wtime"
+                type="date"
+                placeholder="选择日期">
+              </el-date-picker>
+           </el-col>
+           <el-col :span="16">
+              <el-button icon="el-icon-search" circle></el-button>
+              <el-button type="primary" icon="el-icon-edit" circle></el-button>
+              <el-button type="success" icon="el-icon-check" circle></el-button>
+              <el-button type="info" icon="el-icon-message" circle></el-button>
+              <el-button type="warning" icon="el-icon-star-off" circle></el-button>
+              <el-button type="danger" icon="el-icon-delete" circle></el-button>
+           </el-col>
+        </el-row>
+         
+      </el-header>
+      <el-main>
+         <el-table
+            :data="tableData"
+            border
+            style="width: 100%;">
+            <el-table-column
+              prop="date"
+              label="日期"
+              width="180">
+            </el-table-column>
+            <el-table-column
+              prop="name"
+              label="姓名"
+              width="180">
+            </el-table-column>
+            <el-table-column
+              prop="address"
+              label="地址">
+            </el-table-column>
+            <el-table-column
+              label="操作">
+             <template slot-scope="scope">
+                <el-button
+                  @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+                <el-button
+                  type="danger"
+                  @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+             </template>
+            </el-table-column>
+          </el-table>
+      </el-main>
+    </el-container>
+  </el-container>
+ 
+  
   </div>
 </template>
-
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
+  name: "HelloWorld",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: "Welcome to Your Vue.js App",
+      wtime: '',
+      tableData:  [{
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-08',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-06',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-07',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }]
+    };
+  },
+  methods: {
+    handleOpen(e) {
+      console.log(e);
+    },
+    handleClose(e) {
+      console.log(e);
+    },
+    handleEdit(row) {
+      console.log(row);
+    },
+    handleDelete(row) {
+      console.log(row);
     }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+*{
+  margin:0;
+}
+h1,
+h2 {
   font-weight: normal;
 }
 ul {
@@ -37,4 +175,13 @@ li {
 a {
   color: #42b983;
 }
+.el-header {
+    background-color: rgb(2, 4, 7);
+    color: rgb(255, 255, 255);
+    line-height: 60px;
+  }
+  
+  .el-aside {
+    color: #333;
+  }
 </style>
