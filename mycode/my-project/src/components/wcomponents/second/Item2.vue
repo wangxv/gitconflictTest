101bd2fe751b6132d7b6a2563326a9e55cfaf91c
wangxv <1564442827@qq.com>
2018-08-10 15:12:26 +0800

    w-

--- mycode/my-project/src/components/wcomponents/second/Item2.vue
+++ mycode/my-project/src/components/wcomponents/second/Item2.vue
@@ -25,7 +25,8 @@ export default {
           { name: '标签二', type: 'success' },
           { name: '标签三', type: 'info' },
           { name: '标签四', type: 'warning' },
-          { name: '标签五', type: 'danger' }
+          { name: '标签五', type: 'danger' },
+          { name: '标签六', type: 'danger' }
         ]
     };
   },
